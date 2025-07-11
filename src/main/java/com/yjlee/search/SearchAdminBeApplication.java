18f769e2aac867967aacd77e6c2d913215565e7e
hihisososo <hihisososo@naver.com>
2025-07-11 20:25:13 +0900

    Initial commit

---
+++ src/main/java/com/yjlee/search/SearchAdminBeApplication.java
@@ -0,0 +1,13 @@
+package com.yjlee.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchAdminBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchAdminBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yjlee/search/SearchAdminBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjlee.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchAdminBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
