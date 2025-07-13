a0f8675cdf39c54359018828c175a243de663676
hihisososo <hihisososo@naver.com>
2025-07-13 13:44:18 +0900

    chore: 코드포매팅 룰 추가

--- src/main/java/com/yjlee/search/SearchAdminBeApplication.java
+++ src/main/java/com/yjlee/search/SearchAdminBeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SearchAdminBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SearchAdminBeApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SearchAdminBeApplication.class, args);
+  }
 }
