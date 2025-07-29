ab9dc12b54a5529b976d7e80478f194ee4a777b7
hihisososo <hihisososo@naver.com>
2025-07-29 20:44:57 +0900

    ci : 배포 테스트 (#28)

--- src/main/java/com/yjlee/search/config/AwsConfig.java
+++ src/main/java/com/yjlee/search/config/AwsConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.*;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.ssm.SsmClient;
 
 @Slf4j
