1e5abe7d1952d98b7a27a9062f8f6c18e9e58289
hihisososo <hihisososo@naver.com>
2025-09-08 09:07:54 +0900

    feat: 검색어 자동 생성 기능 추가

---
+++ src/main/java/com/yjlee/search/loggen/repository/SearchQueryPoolRepository.java
@@ -0,0 +1,16 @@
+package com.yjlee.search.loggen.repository;
+
+import com.yjlee.search.loggen.model.SearchQueryPool;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SearchQueryPoolRepository extends JpaRepository<SearchQueryPool, Long> {
+
+  boolean existsByQuery(String query);
+
+  @Query(value = "SELECT * FROM search_query_pool ORDER BY RAND() LIMIT 1", nativeQuery = true)
+  Optional<SearchQueryPool> findRandomQuery();
+}
